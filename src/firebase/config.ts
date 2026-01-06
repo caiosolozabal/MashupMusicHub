effb8f0852ebe06d606ec96e7ba7306b38933b85
Caio Luiz Solozabal <clsolo91@gmail.com>
2026-01-06 03:47:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "mashup-music-hub",
+  "appId": "1:700921932715:web:34178bd2fd9116a64dc0ee",
+  "apiKey": "AIzaSyCDNFpKdz_S6xwrWSF9M5h-yp9ZqoO6Gtg",
+  "authDomain": "mashup-music-hub.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "700921932715"
+};
