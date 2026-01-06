4bf7c824e152124964ac7b19db15b550d8933fa9
Caio Luiz Solozabal <clsolo91@gmail.com>
2026-01-06 03:24:15 +0000

    Ok, agora precisamos fazer o site voltar a ser funcional. Ele está com e

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,11 +6,11 @@ import { getStorage, FirebaseStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  authDomain: "listeiro-cf302.firebaseapp.com",
+  projectId: "listeiro-cf302",
+  storageBucket: "listeiro-cf302.appspot.com",
+  messagingSenderId: "1083995834879",
+  appId: "1:1083995834879:web:3504338423232155c5f4de",
 };
 
 // Inicialização Singleton para garantir que o Firebase seja inicializado apenas uma vez.
