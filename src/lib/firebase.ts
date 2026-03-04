466e44617036360d6a7d3e5738b80378c525433f
Caio Luiz Solozabal <clsolo91@gmail.com>
2026-03-04 03:58:31 +0000

    Criado, o link é esse: mashup-music-hub.firebasestorage.app Gere o Bash

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,6 @@ const firebaseConfig = {
   apiKey: "AIzaSyCDNFpKdz_S6xwrWSF9M5h-yp9ZqoO6Gtg",
   authDomain: "mashup-music-hub.firebaseapp.com",
   projectId: "mashup-music-hub",
-  // Tenta o novo padrão do Firebase se o antigo der 404
   storageBucket: "mashup-music-hub.firebasestorage.app",
   messagingSenderId: "700921932715",
   appId: "1:700921932715:web:34178bd2fd9116a64dc0ee"
